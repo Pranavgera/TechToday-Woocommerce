8091f346609cf09e06e3e7c97b35d1ed6210e1cb
Pranav Gera <Pranav.gera@dcmail.ca>
2022-02-18 21:27:05 -0500

    Making some changes to color scheme and layout

---
+++ themes/techtoday-website-theme/inc/block-editor.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Functions which enhance the theme by utilizing block editor
+ * @package TechToday
+ */
+function techToday_enqueue_block_editor_assets() {
+    wp_enqueue_script(
+        'block-editor-script',
+        get_template_directory_uri() .'/assets/js/block-editor.js',
+        array( 'wp-blocks', 'wp-dom-ready', 'wp-edit-post' ),
+        filemtime( get_template_directory() .'/assets/js/block-editor.js')
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'techToday_enqueue_block_editor_assets' );
+
+function techToday_enqueue_block_assets() {
+    wp_enqueue_style(
+    'block-editor-style',
+    get_template_directory_uri() .'/assets/css/block-editor.css',
+    );
+}
+add_action( 'enqueue_block_assets', 'techToday_enqueue_block_assets' );
--- themes/techtoday-website-theme/inc/customizer.php
+++ themes/techtoday-website-theme/inc/customizer.php
@@ -55,6 +55,6 @@ function techtoday_website_theme_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function techtoday_website_theme_customize_preview_js() {
-	wp_enqueue_script( 'techtoday-website-theme-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'techtoday-website-theme-customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), TECHTODAY_VERSION, true );
 }
 add_action( 'customize_preview_init', 'techtoday_website_theme_customize_preview_js' );
