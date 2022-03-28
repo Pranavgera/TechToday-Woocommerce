5c492793fd1a00cdc926f8640c08220b02372ed2
Pranav Gera <Pranav.gera@dcmail.ca>
2022-03-28 19:35:23 -0400

    Added content archive and edited index.php

--- themes/techtoday-website-theme/index.php
+++ themes/techtoday-website-theme/index.php
@@ -15,7 +15,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main grid-container">
 
 		<?php
 		if ( have_posts() ) :
@@ -37,7 +37,7 @@ get_header();
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				get_template_part( 'template-parts/content-archive', get_post_type() );
 
 			endwhile;
 
